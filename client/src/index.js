3def33187f424a60482c60e80a0074200361d3e9
Vincent Wang <a36168120@gmail.com>
2019-07-22 16:30:43 -0700

    Setup Router for App

--- client/src/index.js
+++ client/src/index.js
@@ -3,21 +3,24 @@ import ReactDOM from 'react-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 // @TODO: Uncomment each module as needed in your client app
-// import { ApolloProvider } from 'react-apollo'
-// import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter } from 'react-router-dom'
 // import { Provider as ReduxProvider } from 'react-redux'
 // -------------------------------
 
 import registerServiceWorker from './registerServiceWorker';
 import theme from './theme';
+import client from './apollo'
+import AppRoutes from './routes'
+
 
 /**
  * @TODO: Initialize Apollo Client
  *
  * Uncomment the following line when Apollo Client is configured:
- *
- * import client from './apollo'
- *
+ */
+
+ /*
  * Below in your <App />, wrap your pages in an <ApolloProvider /> component
  * and pass it `client` as the `client` prop value so they will
  * have access to data exposed by your GraphQL API.
@@ -26,9 +29,8 @@ import theme from './theme';
 /**
  * @TODO: Add Routing
  *
- * Uncomment the following line when your routes are configured
+ * 
  *
- * import AppRoutes from './routes'
  *
  * Below in your <App />, nest your <AppRoutes /> inside of <BrowserRouter />
  * component to enable routing in your client app.
@@ -56,17 +58,18 @@ import theme from './theme';
  * user is currently logged in and who that user is.
  */
 
-// @TODO: Remove this import once you have your router working below
-import Home from './pages/Home';
-// -------------------------------
-
 import './index.css';
 
 const App = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
-      <Home />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <AppRoutes />
+        </ BrowserRouter>
+      </ApolloProvider>
+      
     </MuiThemeProvider>
   );
 };
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,5 +1,9 @@
 import React, { Fragment } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
+import HomeContainer from '../pages/Home';
+import ItemsContainer from '../pages/Items';
+import ProfileContainer from '../pages/Profile';
+import ShareContainer from '../pages/Share';
 
 export default () => (
   <Fragment>
@@ -13,6 +17,12 @@ export default () => (
        * Later, we'll add logic to send users to one set of routes if they're logged in,
        * or only view the /welcome page if they are not.
        */}
+      <Route exact path = '/welcome' component = {HomeContainer} />
+      <Route exact path = '/items' component = {ItemsContainer} />
+      <Route exact path = '/profile' componet = {ProfileContainer} />
+      <Route exact path = '/profile/:userid' component = {ProfileContainer} />
+      <Route exact path = '/share' component = {ShareContainer} />
+      <Redirect from = '' to = '/items' />
     </Switch>
   </Fragment>
 );
