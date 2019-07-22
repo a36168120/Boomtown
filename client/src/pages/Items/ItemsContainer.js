4cbefd36845d8c92a8f7a098c35ee5ea4a9c2d84
Vincent Wang <a36168120@gmail.com>
2019-07-22 16:34:29 -0700

    Setup ItemsContainier

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
-import Items from './Items';
+import React, { Component } from "react";
+import Items from "./Items";
 // import FullScreenLoader from '../../components/FullScreenLoader';
-// import { Query } from 'react-apollo';
-// import { } from '../../apollo/queries';
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { ALL_ITEMS_QUERY } from "../../apollo/queries";
 
 class ItemsContainer extends Component {
   render() {
-    return <Items />;
+    return (
+      <div>
+        <Query query={ALL_ITEMS_QUERY} variables={{ filter: 1 }}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
+            return <Items items={data.items} />;
+          }}
+        </Query>
+      </div>
+    );
   }
 }
 
