ab46b121425af391cf3d1069d5a35ca58de76f04
Vincent Wang <a36168120@gmail.com>
2019-07-29 04:57:56 -0700

    update itemcard with style and import viewer

--- client/src/components/ItemCard/Itemcard.js
+++ client/src/components/ItemCard/Itemcard.js
@@ -2,32 +2,60 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 import {
+  Avatar,
   Button,
   Card,
   CardActions,
   CardContent,
   CardHeader,
-  Typography
+  Typography,
+  CardMedia
 } from "@material-ui/core";
-
+import moment from "moment";
 import Gravatar from "react-gravatar";
+import { ViewerContext } from "../../context/ViewerProvider";
 
-const ItemCard = ({ item }) => {
+const ItemCard = ({ item, classes }) => {
   return (
-    <Card>
-      <CardHeader
-        title={item.itemowner.fullname}/>
-        <Gravatar email={item.itemowner.email} />
-      <CardContent>
-        <Typography>{item.title}</Typography>
+    <ViewerContext.Consumer>
+      {({ viewer }) => (
+        <Card className={classes.card}>
 
-        <Typography>{item.description}</Typography>
-      </CardContent>
+          <CardMedia  image={item.imageurl} className={classes.media}/>
 
-      <CardActions>
-        <Button>borrow</Button>
-      </CardActions>
-    </Card>
+          <CardHeader 
+          
+            title= {(item.itemowner && item.itemowner.fullname) || viewer.fullname}
+
+            avatar = {
+              <Avatar round="true" className = {classes.avatar}>
+                <Gravatar email = {(item.itemowner && item.itemowner.email) || viewer.email}/>
+              </Avatar>
+            }
+
+            subheader = {moment(item.created).fromNow()}
+          />
+         
+          <section className = {classes.content}>
+            <CardContent>
+              <Typography component = "h2" variant = "h5" className = {classes.text}> {item.title} </Typography>
+
+              <Typography variant = "body2" color = "textSecondary" component = "p" className = {classes.text}>
+                {item.tags && item.tags.map(tag => tag.title).join(", ")}
+              </Typography>
+
+              <Typography component = "h3" className = {classes.text}> {item.description} </Typography>
+            </CardContent>
+
+            <CardActions>
+              <Button variant = "outlined" className = {classes.button}>
+                borrow
+              </Button>
+            </CardActions>
+          </section>
+        </Card>
+      )}
+    </ViewerContext.Consumer>
   );
 };
 
---
+++ client/src/components/ItemCard/styles.js
@@ -0,0 +1,32 @@
+const styles = theme => ({
+  
+  avatar: {
+    width: '50px',
+    height: '50px'
+  },
+
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between"
+  },
+
+  media: {
+    height: "200px"
+  },
+
+  content: {
+    marginLeft: "10px"
+  },
+
+  text: {
+    marginBottom: '5px'
+  },
+
+  button: {
+    marginBottom: '10px'
+  }
+});
+
+export default styles;
