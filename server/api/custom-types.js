6be28b1b27de805ff346a5bf7a43337170cd3ffb
Vincent Wang <a36168120@gmail.com>
2019-07-29 05:15:23 -0700

    Setup Date functionality

--- server/api/custom-types.js
+++ server/api/custom-types.js
@@ -14,8 +14,13 @@ const { GraphQLScalarType } = require('graphql');
  */
 
 // @TOOD: Refactor this into a custom DATE scalar type using new GraphQLScalarType()
-const DateScalar = undefined;
-// -------------------------------
+const DateScalar = new GraphQLScalarType({
+  name: "DateTime",
+  description: "A date and time, represented as an ISO-8601 string",
+  serialize: value => value.toISOString(),
+  parseValue: value => new Date(value),
+  parseLiteral: ast => new Date(ast.value)
+});
 
 module.exports = {
   DateScalar
