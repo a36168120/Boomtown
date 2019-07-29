b7e94001d6914cc4ec7d9428d14af8c0f8316cb1
Vincent Wang <a36168120@gmail.com>
2019-07-29 05:09:50 -0700

    Update Items page

--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import styles from './styles';
-import ItemsGrid from '../../components/ItemsGrid/ItemGrid';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
+import ItemsGrid from "../../components/ItemsGrid/ItemGrid";
 
 const Items = ({ classes, items }) => {
   return (
-    <div>
-      <p>
-        This is the items page located at <code>/items</code>.
-      </p>
-      <ItemsGrid key = {items.index} items = {items} />
+    <div className = {classes.root}>
+      <ItemsGrid key={items.index} items={items} />
     </div>
   );
 };
 
 export default withStyles(styles)(Items);
+
--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -9,7 +9,7 @@ class ItemsContainer extends Component {
   render() {
     return (
       <div>
-        <Query query={ALL_ITEMS_QUERY} variables={{ filter: 1 }}>
+        <Query query={ALL_ITEMS_QUERY} variables={{ filter: 2 }}>
           {({ loading, error, data }) => {
             if (loading) return "Loading...";
             if (error) return `Error! ${error.message}`;
